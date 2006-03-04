8bd2d337292201e6da8f5a04e82a0e957b4dfeae
Henry BBS <bbs@henry.puli61-10-10.ncnu.edu.tw>
2006-03-04 14:18:58 +0800

    Modify init.d script.

--- sample/pttbbs.sh
+++ sample/pttbbs.sh
@@ -5,28 +5,32 @@
 
 case "$1" in
 start)
+	echo -n 'start...'
 	# 初始化 shared-memory, 載入 uhash, utmpsortd, timed(if necessary)
 	# 如果使用 USE_HUGETLB 的話請用 root 跑 shmctl init
-	/usr/bin/su -fm bbs -c '/home/bbs/bin/shmctl init'
+	/usr/bin/sudo -u bbs /home/bbs/bin/shmctl init > /dev/null
 
 	# 寄信至站外
-	/usr/bin/su -fm bbs -c /home/bbs/bin/outmail &
+	/usr/bin/sudo -u bbs /home/bbs/bin/outmail &
 
 	# 轉信
-	/usr/bin/su -fm bbs -c /home/bbs/innd/innbbsd &
+	/usr/bin/sudo -u bbs /home/bbs/innd/innbbsd &
 
 	# 啟動 port 23 (port 23須使用 root 才能進行 bind ) 以其他
-	/home/bbs/bin/bbsctl start
+	#/home/bbs/bin/bbsctl start
+	/usr/bin/sudo -u bbs /home/bbs/bin/mbbsd 3000
 
 	# 提示
-	echo -n ' mbbsd'
+	echo ' mbbsd'
 	;;
 stop)
+	echo -n 'stop...'
 	/usr/bin/killall outmail
 	/usr/bin/killall innbbsd
 	/usr/bin/killall mbbsd
 	/usr/bin/killall shmctl
-	/bin/sleep 2; /usr/bin/killall shmctl
+	/bin/sleep 2; /usr/bin/killall shmctl 2> /dev/null
+	echo ' mbbsd'
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
