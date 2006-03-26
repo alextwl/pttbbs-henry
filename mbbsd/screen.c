da81a24da65ced64b37e46001c3ba7e3853033d7
kcwu <kcwu@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2006-03-26 16:14:20 +0000

    fix crash, workaround for incorrect roll after resize.
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3306 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/screen.c
+++ mbbsd/screen.c
@@ -1,4 +1,4 @@
-/* $Id: screen.c 3304 2006-03-26 16:14:01Z kcwu $ */
+/* $Id: screen.c 3306 2006-03-26 16:14:20Z kcwu $ */
 #include "bbs.h"
 
 #define o_clear()     output(clearbuf,clearbuflen)
@@ -44,7 +44,7 @@ static inline
 screenline_t* GetCurrentLine(){
     register int i = cur_ln + roll;
     if(i >= scr_lns)
-	i -= scr_lns;
+	i %= scr_lns;
     return &big_picture[i];
 }
 
