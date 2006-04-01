e0c54f9c7c03e0f80bae9a1d727407ca606f3de4
kcwu <kcwu@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2006-04-01 14:33:55 +0000

    more crash assertion.
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3322 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/talk.c
+++ mbbsd/talk.c
@@ -1,4 +1,4 @@
-/* $Id: talk.c 3319 2006-04-01 14:28:03Z kcwu $ */
+/* $Id: talk.c 3322 2006-04-01 14:33:55Z kcwu $ */
 #include "bbs.h"
 
 #define QCAST   int (*)(const void *, const void *)
@@ -3149,6 +3149,8 @@ talkreply(void)
     clear();
 
     outs("\n\n");
+    // FIXME CRASH here
+    assert(sig>=0 && sig<sizeof(sig_des)/sizeof(sig_des[0]));
     prints("       (Y) 讓我們 %s 吧！"
 	    "     (A) 我現在很忙，請等一會兒再 call 我\n", sig_des[sig]);
     prints("       (N) 我現在不想 %s"
