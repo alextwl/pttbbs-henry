acf64421bf12256232ea9764e4949b6ea50e3931
kcwu <kcwu@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2006-04-08 14:20:29 +0000

    fix unpaired brace in comment
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3326 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/board.c
+++ mbbsd/board.c
@@ -1,4 +1,4 @@
-/* $Id: board.c 3325 2006-04-08 14:20:21Z kcwu $ */
+/* $Id: board.c 3326 2006-04-08 14:20:29Z kcwu $ */
 #include "bbs.h"
 
 /* personal board state
@@ -1062,7 +1062,7 @@ choose_board(int newflag)
 	    break;
 
 	case 'Z':
-	    if (HasUserPerm(PERM_LOGINOK)) {
+	    if (HasUserPerm(PERM_LOGINOK))
 		vmsg("為避免誤按此功\能已取消，請改至個人設定區修改設定");
 	    break;
 
