e7566a528a5c5686fc088df769c3965efbeef7ae
victor <victor@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2006-12-03 03:21:09 +0000

    WARNING: SHM_t changed
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3450 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- include/pttstruct.h
+++ include/pttstruct.h
@@ -1,4 +1,4 @@
-/* $Id: pttstruct.h 3449 2006-11-28 05:53:40Z victor $ */
+/* $Id: pttstruct.h 3450 2006-12-03 03:21:09Z victor $ */
 #ifndef INCLUDE_STRUCT_H
 #define INCLUDE_STRUCT_H
 
@@ -561,9 +561,7 @@ typedef struct {
     time4_t close_vote_time;
 
     /* pttcache */
-    char    notes[MAX_MOVIE][200*11];
-    // 有人反應 200 不夠用，將來可能會提高
-    char    this_is_unused_and_should_be_removed_later[499][200*11];
+    char    notes[MAX_MOVIE][256*11];
     char    gap_18[sizeof(int)];
     char    today_is[20];
     int     n_notes[MAX_MOVIE_SECTION];      /* 一節中有幾個 看板 */
