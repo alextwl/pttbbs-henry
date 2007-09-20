ab5ba953c99f95f95d09271d35afd80a9506b232
kcwu <kcwu@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2007-09-20 15:34:36 +0000

    * workaround for some account which has no home dir
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3562 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/mbbsd.c
+++ mbbsd/mbbsd.c
@@ -1,4 +1,4 @@
-/* $Id: mbbsd.c 3545 2007-06-18 17:14:32Z kcwu $ */
+/* $Id: mbbsd.c 3562 2007-09-20 15:34:36Z kcwu $ */
 #ifdef DEBUG
 #define TELOPTS
 #define TELCMDS
@@ -758,9 +758,10 @@ login_query(void)
 			PERM_CLOAK | PERM_SEECLOAK | PERM_XEMPT |
 			PERM_SYSOPHIDE | PERM_BM | PERM_ACCOUNTS |
 			PERM_CHATROOM | PERM_BOARD | PERM_SYSOP | PERM_BBSADM;
-		    mkuserdir(cuser.userid);
 #endif
 		}
+		/* 早該有 home 了, 不知道為何有的帳號會沒有, 被砍掉了? */
+		mkuserdir(cuser.userid);
 		break;
 	    }
 	}
