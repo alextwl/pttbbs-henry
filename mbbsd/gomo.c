c5ac97faa9a84fbcc8d3cdde6a6a7813be87b74e
scw <scw@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2007-10-03 08:27:44 +0000

    Use fency game board in GO and GOMOKU
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3582 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/gomo.c
+++ mbbsd/gomo.c
@@ -1,4 +1,4 @@
-/* $Id: gomo.c 3523 2007-05-30 15:42:18Z scw $ */
+/* $Id: gomo.c 3582 2007-10-03 08:27:44Z scw $ */
 #include "bbs.h"
 #include "gomo.h"
 
@@ -279,12 +279,13 @@ static void
 gomo_drawline(const ChessInfo* info, int line)
 {
     const static char* const BoardPic[] = {
-	"¢z", "¢s", "¢{",
-	"¢u", "¢q", "¢t",
-	"¢|", "¢r", "¢}"
+	"ùİ", "ùç", "ùç", "ùß",
+	"ùò", "¢q", "¢q", "ùô",
+	"ùò", "¢q", "¡Ï", "ùô",
+	"ùã", "ùí", "ùí", "ùå",
     };
     const static int BoardPicIndex[] =
-    { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
+    { 0, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 3 };
 
     board_p board = (board_p) info->board;
 
@@ -298,8 +299,7 @@ gomo_drawline(const ChessInfo* info, int line)
     } else if (line >= 2 && line <= 16) {
 	const int board_line = line - 2;
 	const char* const* const pics =
-	    board_line == 0  ? &BoardPic[0] :
-	    board_line == BRDSIZ - 1 ? &BoardPic[6] : &BoardPic[3];
+	    &BoardPic[BoardPicIndex[board_line] * 4];
 	int i;
 
 	prints("%3d" ANSI_COLOR(30;43), 17 - line);
