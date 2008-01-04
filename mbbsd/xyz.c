c21054ae1be79f877b2608fe7ce0cebeb4b98710
piaip <piaip@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2008-01-04 05:45:20 +0000

    - xyz: show userplan for mailing sysops
    git-svn-id: http://opensvn.csie.org/pttbbs/trunk/pttbbs@3784 63ad8ddf-47c3-0310-b6dd-a9e9d9715204

--- mbbsd/xyz.c
+++ mbbsd/xyz.c
@@ -1,4 +1,4 @@
-/* $Id: xyz.c 3508 2007-05-02 03:39:36Z victor $ */
+/* $Id: xyz.c 3784 2008-01-04 05:45:20Z piaip $ */
 #include "bbs.h"
 
 #if 0
@@ -320,8 +320,10 @@ mail_sysop(void)
 		genbuf, 4, DOECHO);
 	i = genbuf[0] - '0' - 1;
 	if (i >= 0 && i < j) {
+	    char *suser = sysoplist[i].userid;
 	    clear();
-	    do_send(sysoplist[i].userid, NULL);
+	    showplans(suser);
+	    do_send(suser, NULL);
 	}
     }
 }
